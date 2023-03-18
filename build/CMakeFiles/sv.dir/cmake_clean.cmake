eb6c20e3af18feb8ef86950947cb911b24c28307
matsouto <matsouto55@gmail.com>
2023-03-18 16:24:49 +0100

    first commit

---
+++ build/CMakeFiles/sv.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sv.dir/main.cpp.o"
+  "CMakeFiles/sv.dir/main.cpp.o.d"
+  "sv"
+  "sv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/sv.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for sv.
+# This may be replaced when dependencies are built.
