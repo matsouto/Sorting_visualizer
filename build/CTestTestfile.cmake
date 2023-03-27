d3e8e17f1d3434a0a45498b838ccf70addfeb156
matsouto <matsouto55@gmail.com>
2023-03-27 21:03:29 +0200

    insertion sort

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/matsouto/Documents/Code/Cpp/Sorting_Visualizer
+# Build directory: /home/matsouto/Documents/Code/Cpp/Sorting_Visualizer/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
